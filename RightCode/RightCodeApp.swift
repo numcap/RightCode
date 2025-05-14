4c9d5c9252583c903c103a7ef6d9c82035857915
Jonathan <juno9593@gmail.com>
2025-05-14 15:53:17 -0400

    Initial Commit

---
+++ RightCode/RightCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightCodeApp.swift
+//  RightCode
+//
+//  Created by Jonathan Ishak on 2025-05-14.
+//
+
+import SwiftUI
+
+@main
+struct RightCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
