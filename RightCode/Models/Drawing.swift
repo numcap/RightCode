3c2f2b73ece9f976f70b1fb77d2d4953bcb76ddd
Jonathan <juno9593@gmail.com>
2025-06-17 16:47:11 -0400

    refactored the Drawing Model

--- RightCode/Models/Drawing.swift
+++ RightCode/Models/Drawing.swift
@@ -13,7 +13,7 @@ import PencilKit
     var title: String = ""
     var language: Language = .python
     var createdAt: Date = Date()
-    var drawing: PKDrawing?
+    var drawing: PKDrawing = PKDrawing()
     
     init(id: UUID = UUID(), title: String, date: Date, language: Language) {
         self.id = id
@@ -22,14 +22,16 @@ import PencilKit
         self.language = language
     }
     
+    init(id: UUID = UUID(), title: String, date: Date, language: Language, drawing: PKDrawing) {
+        self.id = id
+        self.title = title
+        self.createdAt = date
+        self.language = language
+        self.drawing = drawing
+    }
+    
     func createImage() -> UIImage {
-//        UIGraphicsBeginImageContextWithOptions(CGSize(width: 100, height: 100), false, 0.0)
-//        guard let context = UIGraphicsGetCurrentContext() else { return UIImage() }
-        
-        if let drawing = self.drawing {
-            return drawing.image(from: CGRect(x: 0, y: 0, width: 100, height: 100), scale: 1)
-        }
-        return UIImage()
+        return drawing.image(from: CGRect(x: 0, y: 0, width: 1000, height: 1050), scale: 3)
     }
     
     init() {
